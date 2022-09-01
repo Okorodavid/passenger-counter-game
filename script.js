34538556ef41fbc1b8003e42a4765d7611f84ce1
Okoro David <kdelicious062@gmail.com>
2022-09-01 18:39:09 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+
+let countEl = document.getElementById("counter")
+
+console.log(countEl)
+
+let count = 0
+function test() {
+   count = count + 1
+   countEl.innerText = count
+   console.log(count)
+}
+
+
+
+// let myage = 21
+// let humandogratio =7
+// let mydogage = myage * humandogratio
+// let myDogAge = 50
+// console.log(myDogAge)
+// myDogAge = myDogAge +50
+// console.log(myDogAge)
+// myDogAge = myDogAge -75
+// console.log(myDogAge)
+// myDogAge = myDogAge +45
+// console.log(myDogAge)
+
+// let lap1 = 34
+// let lap2 = 33
+// let lap3 = 36
+
+// let lapAll = lap1+lap2+lap3
+
+// function test() {
+    
+//     console.log(lapAll)
+// }
+
+// test()
+
+// let lapsCompleted = 0
+
+// function lapsComplete() {
+//     lapsCompleted = lapsCompleted + 1
+// }
+
+// lapsComplete()
+// lapsComplete()
+// lapsComplete()
+
+// console.log(lapsCompleted)
