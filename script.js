6f9f6c12fbd160fe982f7d65dc68834f44c96f0c
Okoro David <kdelicious062@gmail.com>
2022-09-12 14:57:20 +0100

    updated

--- script.js
+++ script.js
@@ -1,50 +1,16 @@
+let numberEl = document.getElementById("number")
+let saveEnt = document.getElementById("entries")
+let counter = 0
 
-let countEl = document.getElementById("counter")
-
-console.log(countEl)
-
-let count = 0
-function test() {
-   count = count + 1
-   countEl.innerText = count
-   console.log(count)
+function count() {
+   counter += 1
+   numberEl.textContent = counter 
 }
 
+function saveit() {
+   let saving = counter + " - "
+   saveEnt.textContent += saving
+   counter = 0
+   numberEl.textContent = 0
+}
 
-
-// let myage = 21
-// let humandogratio =7
-// let mydogage = myage * humandogratio
-// let myDogAge = 50
-// console.log(myDogAge)
-// myDogAge = myDogAge +50
-// console.log(myDogAge)
-// myDogAge = myDogAge -75
-// console.log(myDogAge)
-// myDogAge = myDogAge +45
-// console.log(myDogAge)
-
-// let lap1 = 34
-// let lap2 = 33
-// let lap3 = 36
-
-// let lapAll = lap1+lap2+lap3
-
-// function test() {
-    
-//     console.log(lapAll)
-// }
-
-// test()
-
-// let lapsCompleted = 0
-
-// function lapsComplete() {
-//     lapsCompleted = lapsCompleted + 1
-// }
-
-// lapsComplete()
-// lapsComplete()
-// lapsComplete()
-
-// console.log(lapsCompleted)
